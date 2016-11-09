6765e5e2150807b4d107f7f124207174308397e2
Mstiekema <merstiekema@gmail.com>
2016-11-09 20:20:02 +0100

    Added a chat logger
    Automatically logs every message send in chat. Makes a special file for
    each person in the logs folder.
    I'm planning to improve this feature, but this is an early thing to try
    some things out. If you don't want to use it, just quote it out.

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 var tmi = require('tmi.js');
 var options = require('./config.js')
 var bot = new tmi.client(options);
+var fs = require('fs');
+
+
 bot.connect();
 
 bot.on('connected', function (channel) {
 	console.log("Bot connected to channel")
-	bot.action("mstiekema", "Yucibot connected MrDestructoid")
+	bot.action(channel, "Yucibot connected MrDestructoid")
 });
 
+
+//Basic commands
+//I have to move this to a /commands map and import them from there
 bot.on('message', function (channel, user, message, self) {
 	 if (message.startsWith("!test")) {
     	bot.say(channel, "This is a command xD")
 	   	console.log('Did the thing')
-	   	console.log(channel)
 	}
 	else if (message.startsWith("!twitter")) {
 		bot.say(channel, "Merijn his Twitter is https://www.twitter.com/Mstiekema_")
@@ -29,3 +34,23 @@ bot.on('message', function (channel, user, message, self) {
 		};
 	};
 });
+
+
+// Chat logger
+bot.on('message', function (channel, user, message, self) {
+	var time = new Date();
+	var day = time.getDate();
+	var month = time.getMonth();
+	var year = time.getFullYear();
+	var hours = time.getHours();
+	var minutes = time.getMinutes();
+	var seconds = time.getSeconds();
+	var logTime = [day + '-' + month + '-' + year + ' ' + hours + ':' + minutes + ':' + seconds + ' | ']
+	var file = './logs/_' + user.username + '.json'
+	var log = [ logTime + user.username + ': ' + message  + '\n' ]
+
+	fs.appendFile(file, log, function(err) {
+	   if(err) {
+	    return console.log(err);
+	    }
+})}); 
