06ab6486f30ce27572e95550719ac9de823aed84
Mstiekema <merstiekema@gmail.com>
2016-11-10 19:04:21 +0100

    Example config file
    Just fill in your data and rename the file to config.js

---
+++ example.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	options: {
+		debug: true
+	}, 
+	connection: { 
+		cluster: "aws", 
+		reconnect: true 
+	}, 
+	identity: { 
+		username: "Botname", 
+		password: 'oauth:1234abcd',
+		// You can get your oauth key from https://twitchapps.com/tmi/
+		owUser: 'Battletag-1234'
+		// Use a - instead of a # in your battle tag 
+	}, 
+	channels: ["#TwitchChannel"] }
+
+// Rename this file to config.js!
