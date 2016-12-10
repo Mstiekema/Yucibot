e30f1cd8ecfc234e1d444dadb8b8310c7207ad4a
Mstiekema <merstiekema@gmail.com>
2016-12-10 16:10:04 +0100

    Minor changes

--- website.js
+++ website.js
@@ -3,15 +3,6 @@ var app           = express();
 var path          = require('path');
 var request       = require("request");
 var fs            = require('fs');
-var songlistshit  = './static/json/songlistinfo.json'
-var options       = require('./config.js')
-var ytApiKey      = options.identity.ytApiKey
-
-function removeShit() {
-	fs.readFile(songlistshit, 'utf8', function(err, file) {if (err) {return}
-	var result = file.replace("}]", "}, \n");
-	fs.writeFile(songlistshit, result, 'utf8', function(err) {if (err) {return}})});
-}
 
 app.use(express.static(path.join(__dirname, 'static')));
 
