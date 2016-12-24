f42d8e4824c24b33b9a93c1e88c2b9ed48ee2057
Mstiekema <merstiekema@gmail.com>
2016-12-24 23:44:52 +0100

    Fixed sub module
    Everything is working, I think

--- modules/sub.js
+++ modules/sub.js
@@ -4,15 +4,15 @@ var connect 	= require('../app.js')
 var bot 		= connect.bot
 var options 	= require('../config.js')
 var request 	= require("request");
+var fs 			= require('fs');
 
 module.exports = {
-
 	sub: function () {
 		bot.on("resub", function (channel, username, months, message) {
 		    bot.say(channel, "Thanks " + username + " for resubbing " + months + " months in a row! PogChamp //")
 		});
 		bot.on("subscription", function (channel, username, method) {
-    		bot.say(channel, "Thanks " + username + " for subbing to " + channel "! PogChamp")
+    		bot.say(channel, "Thanks " + username + " for subbing to " + channel.substring(1) + "! PogChamp")
 		});
 	}
 }
