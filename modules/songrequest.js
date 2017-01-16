e650a77b09ee9e80c66b242d84d5cc1c71fa6fae
Mstiekema <merstiekema@gmail.com>
2017-01-16 20:55:54 +0100

    Added user to currentsong

--- modules/songrequest.js
+++ modules/songrequest.js
@@ -72,7 +72,7 @@ module.exports = {
 			if (message.startsWith("!currentsong")) {
 				connection.query('select * from songrequest where playState = 0 AND DATE_FORMAT(time,"%Y-%m-%d") = ?', new Date().toISOString().substr(0, 10), function(err,result){
 					console.log(result)
-					bot.say(channel, "The song that is currently playing is: " + result[0].title + " https://www.youtube.com/watch?v=" + result[0].songid)
+					bot.say(channel, "The song that is currently playing is: " + result[0].title + " requested by: " + result[0].name + " https://www.youtube.com/watch?v=" + result[0].songid)
 				});
 			}
 		});
