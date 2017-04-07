9f25591399fa04058a25459d77b6f2a22094e198
Mstiekema <merstiekema@gmail.com>
2017-04-07 19:56:47 +0200

    Added top 5 emotes to stats page

--- website.js
+++ website.js
@@ -473,27 +473,32 @@ app.get('/stats', function(req, res) {
         var toppoints = result
         func.connection.query('select * from user ORDER BY num_lines DESC', function(err, result) {
           var toplines = result
-          func.connection.query('select title from songrequest', function(err, result) {
-            var songrequest = result.length
-            func.connection.query('select type from adminlogs', function(err, result) {
-              var types = result.map(function(a) {return a.type;})
-              var timeouts = types.filter(function(b) {return b == "timeout"});
-              var bans = types.filter(function(b) {return b == "ban"});
-              var allTimeouts = timeouts.length;
-              var allBans = bans.length;
-              res.render('stats.html', {
-                lines: numLines,
-                user: user,
-                songrequest: songrequest,
-                stream: streams,
-                timeout: allTimeouts,
-                ban: allBans,
-                toppoints: toppoints,
-                toplines: toplines
-              })
-            })
+          func.connection.query('SELECT * FROM emotestats INNER JOIN emotes ON emotestats.id = emotes.emoteId where emotes.type = "twitch" \
+    			ORDER BY emotestats.uses DESC LIMIT 5', function(err, result) {
+            var topemotes = result
+            func.connection.query('select title from songrequest', function(err, result) {
+              var songrequest = result.length
+              func.connection.query('select type from adminlogs', function(err, result) {
+                var types = result.map(function(a) {return a.type;})
+                var timeouts = types.filter(function(b) {return b == "timeout"});
+                var bans = types.filter(function(b) {return b == "ban"});
+                var allTimeouts = timeouts.length;
+                var allBans = bans.length;
+                res.render('stats.html', {
+                  lines: numLines,
+                  user: user,
+                  songrequest: songrequest,
+                  stream: streams,
+                  timeout: allTimeouts,
+                  ban: allBans,
+                  toppoints: toppoints,
+                  toplines: toplines,
+                  topemotes: topemotes
+                });
+              });
+            });
           });
-        })
+        });
       });
     });
   });
